3dc17cb4f9aeff986327cf4b1f8f26f07b12415b
Wesley Henderson <wesley.henderson11@gmail.com>
2022-03-30 11:59:09 -0500

    Added a temporary game state initialization function

--- gamestate.h
+++ gamestate.h
@@ -20,16 +20,18 @@ enum TileState
 struct GameState
 {
     // Game constants
-    const int puzzle_width;
-    const int puzzle_height;
-    const int row_clues[MAX_PUZZLE_LENGTH][CLUE_LENGTH];
-    const int col_clues[MAX_PUZZLE_LENGTH][CLUE_LENGTH];
-    const time_t game_start_time;
+    int puzzle_width;
+    int puzzle_height;
+    int row_clues[MAX_PUZZLE_LENGTH][CLUE_LENGTH];
+    int col_clues[MAX_PUZZLE_LENGTH][CLUE_LENGTH];
+    time_t game_start_time;
     // Game state that can change
     int cursor_x_pos;
     int cursor_y_pos;
     time_t elapsed_game_time;
     enum TileState tiles[MAX_PUZZLE_LENGTH][MAX_PUZZLE_LENGTH];
 };
+
+void initGameState(struct GameState *game_state);
 
 #endif // GAMESTATE_H
--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "gamestate.h"
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
+    struct GameState game_state[1];
+    initGameState(game_state);
+    printf("%d x %d\n", game_state->puzzle_width, game_state->puzzle_height);
     printf("Hello, world!\n");
     return 0;
 }
